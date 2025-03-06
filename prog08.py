8c81fd03bd5bf9523176e5392873d6453b100c62
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-03-06 10:43:45 +0800

    Python Exercises 2

--- prog08.py
+++ prog08.py
@@ -9,3 +9,13 @@ while num <= 100:
     num += 1
 
 print()
+
+# Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+num = 1
+
+while num <= 100:
+    print(num, end=" ")
+    num += 2
+
+print()
